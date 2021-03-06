b7398b1034c4d399cd4d8034dc41b53e7bd25304
Emmanuel Touzery <etouzery@gmail.com>
2021-03-06 19:12:21 +0100

    go for proper decoding for now. and skip calling tshark to speed up devel

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,36 @@
 use crate::tshark_packet::TSharkPacket;
 use itertools::Itertools;
-use serde_json::Value;
+use std::fs::File;
+use std::io::Read;
 use std::process::Command;
 
 mod tshark_packet;
 
 fn main() {
     println!("hello");
-    let tshark_output = Command::new("tshark")
-        .args(&["-r", "/home/emmanuel/dump_afc.pcap", "-Tjson"])
-        .output()
-        .expect("failed calling tshark");
-    if !tshark_output.status.success() {
-        eprintln!("tshark returned error code {}", tshark_output.status);
-        std::process::exit(1);
-    }
-    let output_str =
-        std::str::from_utf8(&tshark_output.stdout).expect("tshark output is not valid utf8");
-    match serde_json::from_str::<Value>(output_str) {
-        Ok(Value::Array(vals)) => {
-            let packets = vals
-                .into_iter()
-                .map(TSharkPacket::new)
-                .collect::<Option<Vec<_>>>()
-                .expect("unexpected tshark json format");
-            handle_packets(&packets)
-        }
-        _ => panic!("tshark output is not valid json"),
+    // let tshark_output = Command::new("tshark")
+    //     .args(&["-r", "/home/emmanuel/dump_afc.pcap", "-Tjson"])
+    //     .output()
+    //     .expect("failed calling tshark");
+    // if !tshark_output.status.success() {
+    //     eprintln!("tshark returned error code {}", tshark_output.status);
+    //     std::process::exit(1);
+    // }
+    // let output_str =
+    //     std::str::from_utf8(&tshark_output.stdout).expect("tshark output is not valid utf8");
+    let mut f = File::open("parsed.json").unwrap();
+    let mut output_str = String::new();
+    f.read_to_string(&mut output_str).unwrap();
+    match serde_json::from_str::<Vec<TSharkPacket>>(&output_str) {
+        Ok(packets) => handle_packets(&packets),
+        Err(e) => panic!(format!("tshark output is not valid json: {:?}", e)),
     }
 }
 
 fn handle_packets(packets: &[TSharkPacket]) {
     let by_stream = packets
         .into_iter()
-        .map(|p| (p.get_tcp_stream(), p))
+        .map(|p| (p.source.layers.tcp.as_ref().map(|t| &t.stream), p))
         .into_group_map();
     println!("{} streams", by_stream.len());
     // for packet in packets {
--- src/tshark_packet.rs
+++ src/tshark_packet.rs
@@ -1,46 +1,31 @@
-use serde_json::Value;
+use serde::de;
+use serde_derive::{Deserialize, Serialize};
 
-// not validated... but at least we check it's a JSON Object.
+#[derive(Deserialize)]
 pub struct TSharkPacket {
-    data: serde_json::Map<String, Value>,
+    #[serde(rename = "_source")]
+    pub source: TSharkSource,
 }
 
-impl TSharkPacket {
-    pub fn new(value: Value) -> Option<TSharkPacket> {
-        match value {
-            Value::Object(data) => Some(TSharkPacket { data }),
-            _ => None,
-        }
-    }
+#[derive(Deserialize)]
+pub struct TSharkSource {
+    pub layers: TSharkLayers,
+}
 
-    fn get_source_layers(&self) -> Option<&serde_json::Map<String, Value>> {
-        self.data
-            .get("_source")
-            .and_then(|src| src.get("layers"))
-            .and_then(Value::as_object)
-    }
+#[derive(Deserialize)]
+pub struct TSharkLayers {
+    pub ip: Option<TSharkIpLayer>,
+    pub tcp: Option<TSharkTcpLayer>,
+}
 
-    pub fn get_ip(&self) -> Option<&serde_json::Map<String, Value>> {
-        self.get_source_layers()
-            .and_then(|layer| layer.get("ip"))
-            .and_then(Value::as_object)
-    }
+#[derive(Deserialize)]
+pub struct TSharkIpLayer {
+    #[serde(rename = "ip.src")]
+    pub ip_src: String,
+}
 
-    pub fn get_ip_src(&self) -> Option<&str> {
-        self.get_ip()
-            .and_then(|ip| ip.get("ip.src"))
-            .and_then(Value::as_str)
-    }
-
-    pub fn get_tcp(&self) -> Option<&serde_json::Map<String, Value>> {
-        self.get_source_layers()
-            .and_then(|layer| layer.get("tcp"))
-            .and_then(Value::as_object)
-    }
-
-    pub fn get_tcp_stream(&self) -> Option<&str> {
-        self.get_tcp()
-            .and_then(|layer| layer.get("tcp.stream"))
-            .and_then(Value::as_str)
-    }
+#[derive(Deserialize)]
+pub struct TSharkTcpLayer {
+    #[serde(rename = "tcp.stream")]
+    pub stream: String,
 }
