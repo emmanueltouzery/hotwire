cde41a03a871cf571b7ffcaa2728fb33a458f254
Emmanuel Touzery <etouzery@gmail.com>
2021-04-13 20:16:42 +0200

    http: some work on body content filenames

--- src/http/http_body_widget.rs
+++ src/http/http_body_widget.rs
@@ -8,6 +8,7 @@ use gdk_pixbuf::prelude::*;
 use gtk::prelude::*;
 use relm::Widget;
 use relm_derive::{widget, Msg};
+use std::borrow::Cow;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::mpsc;
@@ -15,6 +16,13 @@ use std::sync::mpsc;
 const TEXT_CONTENTS_STACK_NAME: &str = "text";
 const IMAGE_CONTENTS_STACK_NAME: &str = "image";
 const BINARY_CONTENTS_STACK_NAME: &str = "binary";
+
+const KNOWN_CONTENT_TYPE_PREFIXES: &[(&str, &str)] = &[
+    ("image/", "image"),
+    ("audio/", "audio"),
+    ("video/", "video"),
+    ("application/", "contents"),
+];
 
 #[derive(Msg, Debug)]
 pub enum Msg {
@@ -119,8 +127,58 @@ impl Widget for HttpBodyWidget {
             }
             Msg::SaveBinaryContents => {
                 println!("save binary contents");
+                let attachment_name = self
+                    .model
+                    .data
+                    .as_ref()
+                    .and_then(|d| {
+                        http_message_parser::get_http_header_value(
+                            &d.other_lines,
+                            "Content-Disposition",
+                        )
+                    })
+                    .and_then(|d| {
+                        d.strip_prefix("attachment: filename=\"")
+                            .and_then(|f| f.strip_suffix("\""))
+                            .map(|f| f.to_string())
+                    });
+                let dialog = gtk::FileChooserNativeBuilder::new()
+                    .action(gtk::FileChooserAction::Save)
+                    .title("Export to...")
+                    .modal(true)
+                    .build();
             }
         }
+    }
+
+    fn filename_from_binary_content_type(content_type: &str) -> Cow<'static, str> {
+        match content_type {
+            "image/jpeg" => {
+                return Cow::Borrowed("image.jpg");
+            }
+            "application/msword" => {
+                return Cow::Borrowed("document.docx");
+            }
+            _ => {}
+        }
+        for (prefix, fname) in KNOWN_CONTENT_TYPE_PREFIXES {
+            if let Some(ext) = content_type.strip_prefix(prefix) {
+                return Cow::Owned(format!(
+                    "{}.{}",
+                    fname,
+                    Self::content_type_extension_cleanup(ext)
+                ));
+            }
+        }
+        Cow::Borrowed("data.bin")
+    }
+
+    fn content_type_extension_cleanup(extension: &str) -> &str {
+        extension
+            .trim_start_matches("x-")
+            .splitn(2, '+')
+            .last()
+            .unwrap()
     }
 
     fn load_image(file_path: &Path, tcp_seq_number: u32, s: relm::Sender<(Vec<u8>, u32)>) {
@@ -178,5 +236,23 @@ impl Widget for HttpBodyWidget {
                }
            }
        }
+    }
+}
+
+#[test]
+pub fn content_type_to_filename_tests() {
+    for (ct, fname) in &[
+        ("application/zip", "contents.zip"),
+        ("application/x-bzip", "contents.bzip"),
+        ("audio/aac", "audio.aac"),
+        ("image/gif", "image.gif"),
+        ("audio/x-midi", "audio.midi"),
+        ("application/epub+zip", "contents.zip"),
+        ("application/msword", "document.docx"),
+    ] {
+        assert_eq!(
+            fname,
+            &HttpBodyWidget::filename_from_binary_content_type(ct)
+        );
     }
 }
