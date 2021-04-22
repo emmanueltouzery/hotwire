0a3c6b9cb9c15eb2edbf9d51cb4109282295088e
Emmanuel Touzery <etouzery@gmail.com>
2021-04-22 23:01:30 +0200

    more work on http2

--- src/http2/http2_message_parser.rs
+++ src/http2/http2_message_parser.rs
@@ -1,3 +1,4 @@
+use crate::http::http_message_parser;
 use crate::icons;
 use crate::message_parser::{MessageInfo, MessageParser, StreamData};
 use crate::tshark_communication::TSharkCommunication;
@@ -19,7 +20,36 @@ impl MessageParser for Http2 {
     }
 
     fn parse_stream(&self, stream: Vec<TSharkCommunication>) -> StreamData {
-        todo!()
+        let mut server_ip = stream
+            .first()
+            .unwrap()
+            .source
+            .layers
+            .ip
+            .as_ref()
+            .unwrap()
+            .ip_dst
+            .clone();
+        let mut server_port = stream
+            .first()
+            .unwrap()
+            .source
+            .layers
+            .tcp
+            .as_ref()
+            .unwrap()
+            .port_dst;
+        let mut summary_details = None;
+        let mut messages = vec![];
+        for msg in stream {
+            dbg!(msg.source.layers.http2);
+        }
+        StreamData {
+            server_ip,
+            server_port,
+            messages,
+            summary_details,
+        }
     }
 
     fn populate_treeview(
@@ -29,7 +59,7 @@ impl MessageParser for Http2 {
         messages: &[MessageData],
         start_idx: i32,
     ) {
-        todo!()
+        http_message_parser::Http.populate_treeview(ls, session_id, messages, start_idx)
     }
 
     fn add_details_to_scroll(
@@ -39,19 +69,22 @@ impl MessageParser for Http2 {
         bg_sender: mpsc::Sender<BgFunc>,
         win_msg_sender: relm::StreamHandle<win::Msg>,
     ) -> Box<dyn Fn(mpsc::Sender<BgFunc>, PathBuf, MessageInfo)> {
-        todo!()
+        http_message_parser::Http.add_details_to_scroll(parent, overlay, bg_sender, win_msg_sender)
     }
 
     fn get_empty_liststore(&self) -> gtk::ListStore {
-        todo!()
+        http_message_parser::Http.get_empty_liststore()
     }
-    fn prepare_treeview(&self, _: &gtk::TreeView) {
-        todo!()
+
+    fn prepare_treeview(&self, tv: &gtk::TreeView) {
+        http_message_parser::Http.prepare_treeview(tv);
     }
+
     fn requests_details_overlay(&self) -> bool {
-        todo!()
+        http_message_parser::Http.requests_details_overlay()
     }
-    fn end_populate_treeview(&self, _: &gtk::TreeView, _: &gtk::ListStore) {
-        todo!()
+
+    fn end_populate_treeview(&self, tv: &gtk::TreeView, ls: &gtk::ListStore) {
+        http_message_parser::Http.end_populate_treeview(tv, ls);
     }
 }
