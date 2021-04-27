5a618f683df7ce7f7a7b09f96a6fdbc1bb7bf5c0
Emmanuel Touzery <etouzery@gmail.com>
2021-04-27 17:20:27 +0200

    http2: take into account streams and combine multiple frames

--- src/http2/http2_message_parser.rs
+++ src/http2/http2_message_parser.rs
@@ -1,11 +1,13 @@
 use crate::http::http_message_parser;
 use crate::http::http_message_parser::{HttpBody, HttpMessageData, HttpRequestResponseData};
+use crate::http2::tshark_http2::TSharkHttp2Message;
 use crate::icons;
 use crate::message_parser::{MessageInfo, MessageParser, StreamData};
-use crate::tshark_communication::TSharkCommunication;
+use crate::tshark_communication::{TSharkCommunication, TSharkFrameLayer, TSharkTcpLayer};
 use crate::widgets::comm_remote_server::MessageData;
 use crate::widgets::win;
 use crate::BgFunc;
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::str;
 use std::sync::mpsc;
@@ -43,79 +45,36 @@ impl MessageParser for Http2 {
             .port_dst;
         let mut summary_details = None;
         let mut messages = vec![];
+        let mut cur_messages_per_stream = HashMap::new();
         let mut cur_request = None;
         for msg in stream {
             if let Some(http2) = msg.source.layers.http2 {
-                let tcp_stream_no = msg.source.layers.tcp.as_ref().unwrap().stream;
-                let tcp_seq_number = msg.source.layers.tcp.as_ref().unwrap().seq_number;
-                let timestamp = msg.source.layers.frame.frame_time;
                 for http2_msg in http2.messages {
-                    let body = http2_msg
-                        .data
-                        .map(|d| {
-                            str::from_utf8(&d)
-                                .ok()
-                                .map(|s| HttpBody::Text(s.to_string()))
-                                .unwrap_or_else(|| HttpBody::Binary(d))
-                        })
-                        .unwrap_or(HttpBody::Missing);
-                    let content_type = http_message_parser::get_http_header_value(
-                        &http2_msg.headers,
-                        "content-type",
-                    )
-                    .cloned();
-                    if http_message_parser::get_http_header_value(&http2_msg.headers, ":status")
-                        .is_none()
-                    {
-                        // every http2 response must contain a ":status" header
-                        // https://tools.ietf.org/html/rfc7540#section-8.1.2.4
-                        // => this is a request
-                        let first_line = format!(
-                            "{} {}",
-                            http_message_parser::get_http_header_value(
-                                &http2_msg.headers,
-                                ":method"
-                            )
-                            .map(|s| s.as_str())
-                            .unwrap_or("-"),
-                            http_message_parser::get_http_header_value(&http2_msg.headers, ":path")
-                                .map(|s| s.as_str())
-                                .unwrap_or("-")
+                    dbg!(&http2_msg);
+                    let stream_msgs_entry = cur_messages_per_stream
+                        .entry(http2_msg.stream_id)
+                        .or_insert(vec![]);
+                    let stream_id = http2_msg.stream_id;
+                    let is_end_stream = http2_msg.is_end_stream;
+                    stream_msgs_entry.push(http2_msg);
+                    if is_end_stream {
+                        let (msg, msg_type) = prepare_http_message(
+                            msg.source.layers.tcp.as_ref().unwrap(),
+                            &msg.source.layers.frame,
+                            cur_messages_per_stream.remove(&stream_id).unwrap(),
                         );
-                        cur_request = Some(HttpRequestResponseData {
-                            tcp_stream_no,
-                            tcp_seq_number,
-                            timestamp,
-                            first_line,
-                            content_type,
-                            headers: http2_msg.headers,
-                            body,
-                        });
-                    } else {
-                        // this is a response
-                        let first_line = format!(
-                            "HTTP/2 status {}",
-                            http_message_parser::get_http_header_value(
-                                &http2_msg.headers,
-                                ":status"
-                            )
-                            .map(|s| s.as_str())
-                            .unwrap_or("-"),
-                        );
-                        let response = Some(HttpRequestResponseData {
-                            tcp_stream_no,
-                            tcp_seq_number,
-                            timestamp,
-                            first_line,
-                            content_type,
-                            headers: http2_msg.headers,
-                            body,
-                        });
-                        messages.push(MessageData::Http(HttpMessageData {
-                            request: cur_request,
-                            response,
-                        }));
-                        cur_request = None;
+                        match msg_type {
+                            MsgType::Request => {
+                                cur_request = Some(msg);
+                            }
+                            MsgType::Response => {
+                                messages.push(MessageData::Http(HttpMessageData {
+                                    request: cur_request,
+                                    response: Some(msg),
+                                }));
+                                cur_request = None;
+                            }
+                        }
                     }
                 }
             }
@@ -169,4 +128,94 @@ impl MessageParser for Http2 {
     fn end_populate_treeview(&self, tv: &gtk::TreeView, ls: &gtk::ListStore) {
         http_message_parser::Http.end_populate_treeview(tv, ls);
     }
+}
+
+enum MsgType {
+    Request,
+    Response,
+}
+
+fn prepare_http_message(
+    tcp: &TSharkTcpLayer,
+    frame: &TSharkFrameLayer,
+    http2_msgs: Vec<TSharkHttp2Message>,
+) -> (HttpRequestResponseData, MsgType) {
+    let (headers, data) = http2_msgs.into_iter().fold(
+        (vec![], None::<Vec<u8>>),
+        |(mut sofar_h, mut sofar_d), mut cur| {
+            sofar_h.append(&mut cur.headers);
+            let new_data = match (sofar_d, cur.data) {
+                (None, Some(d)) => Some(d),
+                (Some(mut s), Some(mut n)) => {
+                    s.append(&mut n);
+                    Some(s)
+                }
+                (d, _) => d,
+            };
+            (sofar_h, new_data)
+        },
+    );
+    let body = data
+        .map(|d| {
+            str::from_utf8(&d)
+                .ok()
+                .map(|s| HttpBody::Text(s.to_string()))
+                .unwrap_or_else(|| HttpBody::Binary(d))
+        })
+        .unwrap_or(HttpBody::Missing);
+
+    let (first_line, msg_type) =
+        if http_message_parser::get_http_header_value(&headers, ":status").is_none() {
+            // every http2 response must contain a ":status" header
+            // https://tools.ietf.org/html/rfc7540#section-8.1.2.4
+            // => this is a request
+            (
+                format!(
+                    "{} {}",
+                    http_message_parser::get_http_header_value(&headers, ":method")
+                        .map(|s| s.as_str())
+                        .unwrap_or("-"),
+                    http_message_parser::get_http_header_value(&headers, ":path")
+                        .map(|s| s.as_str())
+                        .unwrap_or("-")
+                ),
+                MsgType::Request,
+            )
+        } else {
+            // this is a response
+            (
+                format!(
+                    "HTTP/2 status {}",
+                    http_message_parser::get_http_header_value(&headers, ":status")
+                        .map(|s| s.as_str())
+                        .unwrap_or("-"),
+                ),
+                MsgType::Response,
+            )
+        };
+    let content_type =
+        http_message_parser::get_http_header_value(&headers, "content-type").cloned();
+    if matches!(body, HttpBody::Binary(_)) {
+        println!(
+            "######### GOT BINARY BODY {:?} status {:?} path {:?}",
+            content_type,
+            http_message_parser::get_http_header_value(&headers, ":status"),
+            http_message_parser::get_http_header_value(&headers, ":path"),
+        );
+    }
+    let tcp_stream_no = tcp.stream;
+    let tcp_seq_number = tcp.seq_number;
+    let timestamp = frame.frame_time;
+    (
+        HttpRequestResponseData {
+            tcp_stream_no,
+            tcp_seq_number,
+            timestamp,
+            first_line,
+            content_type,
+            headers,
+            body,
+        },
+        msg_type,
+    )
 }
