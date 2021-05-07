f73147afe90c766ac1d3d6648525600fe2528f35
Emmanuel Touzery <etouzery@gmail.com>
2021-05-07 23:16:27 +0200

    xml: progress on pgsql

--- src/pgsql/tshark_pgsql.rs
+++ src/pgsql/tshark_pgsql.rs
@@ -1,9 +1,9 @@
 // https://www.postgresql.org/docs/12/protocol.html
 use crate::tshark_communication;
 use quick_xml::events::Event;
-use std::collections::HashMap;
 use std::io::BufReader;
 use std::process::ChildStdout;
+use std::str;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum PostgresColType {
@@ -32,6 +32,9 @@ pub enum PostgresWireMessage {
         query: Option<String>,
         statement: Option<String>,
     },
+    // for prepared statements, the first time we get parse & statement+query
+    // the following times we get bind and only statement (statement ID)
+    // we can then recover the query from the statement id in post-processing.
     Bind {
         statement: Option<String>,
         parameter_values: Vec<String>,
@@ -67,6 +70,14 @@ fn parse_pgsql_info(
                                 }
                                 b"Copy data" => result.push(PostgresWireMessage::CopyData),
                                 b"Parse" => result.push(parse_parse_message(xml_reader, buf)),
+                                b"Bind" => result.push(parse_bind_message(xml_reader, buf)),
+                                b"Ready for query" => {
+                                    result.push(PostgresWireMessage::ReadyForQuery)
+                                }
+                                b"Row description" => {
+                                    result.push(parse_row_description_message(xml_reader, buf))
+                                }
+                                b"Data row" => result.push(parse_data_row_message(xml_reader, buf)),
                             }
                         }
                     }
@@ -163,107 +174,187 @@ fn parse_parse_message(
     }
 }
 
-fn parse_pg_value(pgsql: &serde_json::Value) -> Option<PostgresWireMessage> {
-    let obj = pgsql.as_object();
-    let typ = obj
-        .and_then(|o| o.get("pgsql.type"))
-        .and_then(|t| t.as_str());
-    // if let Some(query_info) = obj.and_then(|o| o.get("pgsql.query")) {
-    match typ {
-        Some("Bind") => {
-            // for prepared statements, the first time we get parse & statement+query
-            // the following times we get bind and only statement (statement ID)
-            // we can then recover the query from the statement id in post-processing.
-            Some(PostgresWireMessage::Bind {
-                // query: format!("{} -> {}", time_relative, q),
-                statement: obj
-                    .and_then(|o| o.get("pgsql.statement"))
-                    .and_then(|s| s.as_str())
-                    .filter(|s| !s.is_empty())
-                    .map(|s| s.to_string()),
-                parameter_values: obj
-                    .and_then(|o| o.iter().find(|(k, _v)| k.starts_with("Parameter values: ")))
-                    .and_then(|(_k, v)| {
-                        if let serde_json::Value::Object(tree) = v {
-                            let col_lengths: Vec<i32> =
-                                parse_str_or_array(tree.get("pgsql.val.length").unwrap(), |s| {
-                                    s.parse().unwrap_or(0)
-                                });
-                            let raw_cols =
-                                parse_str_or_array(tree.get("pgsql.val.data").unwrap(), |s| {
-                                    hex_chars_to_string(s).unwrap_or_default() // TODO should check pgsql.oid.type.. sometimes i get integers here
-                                });
-                            return Some(add_cols(raw_cols, col_lengths));
+fn parse_bind_message(
+    xml_reader: &quick_xml::Reader<BufReader<ChildStdout>>,
+    buf: &mut Vec<u8>,
+) -> PostgresWireMessage {
+    let mut statement;
+    let mut parameter_values;
+    loop {
+        match xml_reader.read_event(buf) {
+            Ok(Event::Empty(ref e)) => {
+                if e.name() == b"field" {
+                    let name = e
+                        .attributes()
+                        .find(|kv| kv.unwrap().key == "name".as_bytes())
+                        .map(|kv| &*kv.unwrap().value);
+                    match name {
+                        Some(b"pgsql.statement") => {
+                            statement = String::from_utf8(
+                                tshark_communication::element_attr_val(e, b"show").to_vec(),
+                            )
+                            .ok()
+                            .filter(|s| !s.is_empty());
                         }
-                        None
-                    })
-                    .unwrap_or_else(Vec::new),
-            })
-        }
-        Some("Ready for query") => Some(PostgresWireMessage::ReadyForQuery),
-        Some("Row description") => {
-            let tree = obj.unwrap().get("pgsql.field.count_tree").unwrap();
-            let col_names = match tree.get("pgsql.col.name") {
-                Some(serde_json::Value::String(s)) => {
-                    vec![s.to_string()]
+                        Some(b"") => {
+                            let show = String::from_utf8(
+                                tshark_communication::element_attr_val(e, b"show").to_vec(),
+                            )
+                            .ok()
+                            .unwrap();
+                            if show.starts_with("Parameter values") {
+                                parameter_values = parse_parameter_values(xml_reader, buf);
+                            }
+                        }
+                    }
                 }
-                Some(serde_json::Value::Array(ar)) => {
-                    ar.iter().map(|v| v.as_str().unwrap().to_string()).collect()
-                }
-                _ => vec![],
-            };
-            let col_types = match tree.get("pgsql.col.name_tree") {
-                Some(serde_json::Value::Object(o)) => {
-                    vec![o
-                        .get("pgsql.oid.type")
-                        .and_then(|t| t.as_str())
-                        .map(parse_pg_oid_type)
-                        .unwrap_or(PostgresColType::Other)]
-                }
-                Some(serde_json::Value::Array(ar)) => ar
-                    .iter()
-                    .map(|v| {
-                        v.as_object()
-                            .and_then(|o| o.get("pgsql.oid.type"))
-                            .and_then(|t| t.as_str())
-                            .map(parse_pg_oid_type)
-                            .unwrap_or(PostgresColType::Other)
-                    })
-                    .collect(),
-                _ => vec![],
-            };
-            Some(PostgresWireMessage::RowDescription {
-                col_names,
-                col_types,
-            })
-        }
-        Some("Data row") => {
-            let col_count = obj
-                .unwrap()
-                .get("pgsql.field.count")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap();
-            let tree = obj.unwrap().get("pgsql.field.count_tree").unwrap();
-            let col_lengths: Vec<i32> =
-                parse_str_or_array(tree.get("pgsql.val.length").unwrap(), |s| {
-                    s.parse().unwrap_or(0) // TODO the _or(0) is a workaround because we didn't code everything yet
-                });
-
-            let raw_cols = tree
-                .get("pgsql.val.data")
-                // TODO the or_default is a workaround because we didn't code everything yet
-                .map(|t| parse_str_or_array(t, |s| hex_chars_to_string(s).unwrap_or_default()))
-                .unwrap_or_else(Vec::new);
-            let cols = add_cols(raw_cols, col_lengths);
-            if col_count != cols.len() {
-                panic!("{} != {}", col_count, cols.len());
             }
-            Some(PostgresWireMessage::ResultSetRow { cols })
+            Ok(Event::End(ref e)) => {
+                if e.name() == b"proto" {
+                    return PostgresWireMessage::Bind {
+                        statement,
+                        parameter_values,
+                    };
+                }
+            }
         }
-        _ => None,
+    }
+}
+
+fn parse_parameter_values(
+    xml_reader: &quick_xml::Reader<BufReader<ChildStdout>>,
+    buf: &mut Vec<u8>,
+) -> Vec<String> {
+    let mut param_lengths = vec![];
+    let mut param_vals = vec![];
+    loop {
+        match xml_reader.read_event(buf) {
+            Ok(Event::Empty(ref e)) => {
+                if e.name() == b"field" {
+                    let name = e
+                        .attributes()
+                        .find(|kv| kv.unwrap().key == "name".as_bytes())
+                        .map(|kv| &*kv.unwrap().value);
+                    match name {
+                        Some(b"pgsql.val.length") => {
+                            param_lengths.push(
+                                str::from_utf8(tshark_communication::element_attr_val(e, b"show"))
+                                    .unwrap()
+                                    .parse()
+                                    .unwrap(),
+                            );
+                        }
+                        Some(b"pgsql.val.data") => {
+                            param_vals.push(
+                                hex_chars_to_string(
+                                    str::from_utf8(tshark_communication::element_attr_val(
+                                        e, b"show",
+                                    ))
+                                    .unwrap(),
+                                )
+                                .unwrap(),
+                            );
+                        }
+                    }
+                }
+            }
+            Ok(Event::End(ref e)) => {
+                if e.name() == b"proto" {
+                    return add_cols(param_vals, param_lengths);
+                }
+            }
+        }
+    }
+}
+
+fn parse_row_description_message(
+    xml_reader: &quick_xml::Reader<BufReader<ChildStdout>>,
+    buf: &mut Vec<u8>,
+) -> PostgresWireMessage {
+    let mut col_names = vec![];
+    let mut col_types = vec![];
+    loop {
+        match xml_reader.read_event(buf) {
+            Ok(Event::Empty(ref e)) => {
+                if e.name() == b"field" {
+                    let name = e
+                        .attributes()
+                        .find(|kv| kv.unwrap().key == "name".as_bytes())
+                        .map(|kv| &*kv.unwrap().value);
+                    match name {
+                        Some(b"pgsql.col.name") => {
+                            col_names.push(
+                                String::from_utf8(
+                                    tshark_communication::element_attr_val(e, b"show").to_vec(),
+                                )
+                                .unwrap(),
+                            );
+                        }
+                        Some(b"pgsql.oid.type") => {
+                            col_types.push(parse_pg_oid_type(
+                                str::from_utf8(tshark_communication::element_attr_val(e, b"show"))
+                                    .unwrap(),
+                            ));
+                        }
+                    }
+                }
+            }
+            Ok(Event::End(ref e)) => {
+                if e.name() == b"proto" {
+                    return PostgresWireMessage::RowDescription {
+                        col_names,
+                        col_types,
+                    };
+                }
+            }
+        }
+    }
+}
+
+fn parse_data_row_message(
+    xml_reader: &quick_xml::Reader<BufReader<ChildStdout>>,
+    buf: &mut Vec<u8>,
+) -> PostgresWireMessage {
+    let mut col_lengths = vec![];
+    let mut col_vals = vec![];
+    loop {
+        match xml_reader.read_event(buf) {
+            Ok(Event::Empty(ref e)) => {
+                if e.name() == b"field" {
+                    let name = e
+                        .attributes()
+                        .find(|kv| kv.unwrap().key == "name".as_bytes())
+                        .map(|kv| &*kv.unwrap().value);
+                    match name {
+                        Some(b"pgsql.val.length") => {
+                            col_lengths.push(
+                                str::from_utf8(tshark_communication::element_attr_val(e, b"show"))
+                                    .unwrap()
+                                    .parse()
+                                    .unwrap(),
+                            );
+                        }
+                        Some(b"pgsql.val.data") => {
+                            col_vals.push(
+                                hex_chars_to_string(
+                                    str::from_utf8(tshark_communication::element_attr_val(
+                                        e, b"show",
+                                    ))
+                                    .unwrap(),
+                                )
+                                .unwrap(),
+                            );
+                        }
+                    }
+                }
+            }
+            Ok(Event::End(ref e)) => {
+                if e.name() == b"proto" {
+                    let cols = add_cols(col_vals, col_lengths);
+                    return PostgresWireMessage::ResultSetRow { cols };
+                }
+            }
+        }
     }
 }
 
