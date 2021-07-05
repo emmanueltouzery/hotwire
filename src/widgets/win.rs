fbbea140da890c0a4277530ad4839e39e6070e62
Emmanuel Touzery <etouzery@gmail.com>
2021-07-05 16:43:37 +0200

    more robust check whether we're running in flatpak

--- src/widgets/win.rs
+++ src/widgets/win.rs
@@ -55,8 +55,8 @@ pub fn get_message_parsers() -> Vec<Box<dyn MessageParser>> {
 }
 
 pub fn is_flatpak() -> bool {
-    // if $XDG_DATA_HOME is NOT defined, I assume I'm not in a flatpak
-    std::env::var("XDG_DATA_HOME").is_ok()
+    // The Flatpak environment can be detected at runtime by looking for a file named /.flatpak-info. https://github.com/flathub/flathub/wiki/App-Maintenance
+    Path::new("/.flatpak-info").exists()
 }
 
 #[derive(Debug, PartialEq, Eq)]
