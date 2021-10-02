01c8b829c583e9a93da2ba7e3b8ec60135c8a581
Emmanuel Touzery <etouzery@gmail.com>
2021-10-02 22:53:24 +0200

    remove dead code

--- src/widgets/headerbar_search.rs
+++ src/widgets/headerbar_search.rs
@@ -4,7 +4,6 @@ use nom;
 use nom::branch::*;
 use nom::bytes::complete::tag;
 use nom::character::complete::*;
-use nom::combinator::*;
 use nom::multi::*;
 use relm::{Component, Widget};
 use relm_derive::{widget, Msg};
@@ -22,22 +21,6 @@ pub struct Model {
     search_toggle_signal: Option<glib::SignalHandlerId>,
     search_options: Option<Component<SearchOptions>>,
 }
-
-// #[derive(PartialEq, Eq, Debug)]
-// enum SearchOperator {
-//     And,
-//     Or,
-// }
-
-// enum SearchNode<'a> {
-//     String(&'a str),
-//     Contains(&'a str, &'a str),
-//     BinaryExpr {
-//         op: SearchOperator,
-//         lhs: Box<SearchNode<'a>>,
-//         rhs: Box<SearchNode<'a>>,
-//     },
-// }
 
 #[derive(PartialEq, Eq, Debug)]
 enum SearchOperator {
@@ -143,77 +126,6 @@ fn parse_word(input: &str) -> nom::IResult<&str, String> {
     })(input)
 }
 
-// fn search_expression_next_token<'a>(expr: &'a str) -> (Option<Token<'a>>, Option<&'a str>) {
-//     let mut offset = 0;
-//     let mut end_chr = None;
-//     let mut cur_token_start = 0;
-//     for chr in expr.chars() {
-//         match end_chr {
-//             None => {
-//                 // still searching for the token start
-//                 if chr == ' ' {
-//                     offset += 1;
-//                     continue;
-//                 }
-//                 cur_token_start = offset;
-//                 if chr == '"' {
-//                     end_chr = Some('"');
-//                     offset += 1;
-//                 } else {
-//                     end_chr = Some(' ');
-//                 }
-//             }
-//             Some('"') if chr == '"' => {
-//                 // hit the token end
-//                 return (
-//                     Some(Token::String(&expr[(cur_token_start + 1)..(offset - 1)])),
-//                     Some(&expr[offset..]),
-//                 );
-//             }
-//             Some(' ') if chr == ' ' => {
-//                 return (
-//                     Some(parse_simple_token(&expr[cur_token_start..offset])),
-//                     Some(&expr[offset..]),
-//                 );
-//             }
-//             Some(_) => {
-//                 // character within token
-//             }
-//         }
-//         offset += 1;
-//     }
-//     // hit the end of the string
-
-//     match end_chr {
-//         Some(' ') => {
-//             // when we search for the end character ' ',
-//             // we also accept EOF to conclude the token
-//             (
-//                 Some(parse_simple_token(&expr[cur_token_start..offset])),
-//                 None,
-//             )
-//         }
-//         Some('"') => {
-//             // unterminated string, that's an invalid expression
-//             (
-//                 Some(Token::InvalidExpr(&expr[cur_token_start..(offset - 1)])),
-//                 None,
-//             )
-//         }
-//         _ => (None, None),
-//     }
-// }
-
-// fn parse_simple_token<'a>(val: &'a str) -> Token<'a> {
-//     let token = match val {
-//         "contains" => Token::ContainsKeyword,
-//         "and" => Token::AndKeyword,
-//         "or" => Token::OrKeyword,
-//         _ => Token::String(val),
-//     };
-//     token
-// }
-
 #[widget]
 impl Widget for HeaderbarSearch {
     fn init_view(&mut self) {
@@ -283,18 +195,6 @@ impl Widget for HeaderbarSearch {
         }
     }
 
-    // fn parse_search_entry(&self) -> SearchInfo {
-    //     let search_contents = self.widgets.search_entry.text().to_string();
-    //     if search_contents.contains(" grid_cells ") || search_contents.contains(" details ") {
-    //         parse_search_expression(&search_contents)
-    //     } else {
-    //         SearchInfo {
-    //             grid_cells: Some(search_contents),
-    //             detail_contents: None,
-    //         }
-    //     }
-    // }
-
     view! {
         gtk::Box {
             #[style_class="linked"]
@@ -323,43 +223,6 @@ impl Widget for HeaderbarSearch {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // fn get_tokens_vec(expr: &str) -> Vec<Token> {
-    //     let mut res = vec![];
-    //     let mut rest = Some(expr);
-    //     loop {
-    //         let token_rest = search_expression_next_token(rest.unwrap());
-    //         if token_rest.0.is_some() {
-    //             res.push(token_rest.0.unwrap());
-    //         }
-    //         rest = token_rest.1;
-    //         if rest.is_none() {
-    //             return res;
-    //         }
-    //     }
-    // }
-
-    //     #[test]
-    //     fn tokenize_empty_search_expression() {
-    //         assert_eq!(Vec::<Token>::new(), get_tokens_vec(""));
-    //     }
-
-    //     #[test]
-    //     fn tokenize_incomplete_string_search_expression() {
-    //         assert_eq!(vec![Token::InvalidExpr("\"a")], get_tokens_vec("\"a"));
-    //     }
-
-    //     #[test]
-    //     fn tokenize_simple_search_expression() {
-    //         assert_eq!(
-    //             vec![
-    //                 Token::String("grid.cells"),
-    //                 Token::ContainsKeyword,
-    //                 Token::String("test")
-    //             ],
-    //             get_tokens_vec("grid.cells contain \"test\"")
-    //         );
-    //     }
 
     #[test]
     fn parse_quoted_string_simple_case() {
